2ac8262c251087d8c20a9eea1747d9b3713d9259
emmepi92 <mariapiacoccioli@live.it>
2021-06-11 19:30:16 +0200

    better

--- bonus/js/js-bonus.js
+++ bonus/js/js-bonus.js
@@ -1,4 +1,4 @@
-function getRandomNum (min,max) {
+function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -23,7 +23,7 @@ var bomb = 1;
 var maxOfNums = 1;
 
 
-settingInputButton.addEventListener('click', function(){
+settingInputButton.addEventListener('click', function () {
 
     bomb = parseInt(bombHtmlInput.value);
     bombHtmlInput.disabled = true;
@@ -31,78 +31,78 @@ settingInputButton.addEventListener('click', function(){
     maxOfNums = parseInt(maxOfNumsHtmlInput.value);
     maxOfNumsHtmlInput.disabled = true;
 
-    document.getElementById("max").innerHTML ="Inserisci un numero compreso fra 1 e " + maxOfNums;
-    
-    invisible.style.display ='block';
+    document.getElementById("max").innerHTML = "Inserisci un numero compreso fra 1 e " + maxOfNums;
+
+    invisible.style.display = 'block';
     settingInputButton.style.display = 'none';
 
     while (bombList.length < bomb) {
-        var computerNum = getRandomNum (1,maxOfNums);
+        var computerNum = getRandomNum(1, maxOfNums);
         if (!bombList.includes(computerNum)) {
-            bombList.push(computerNum);        
+            bombList.push(computerNum);
         }
-    } 
+    }
     console.log(bombList.sort());
 
 
 })
 
 
-nextStepButton.addEventListener('click', function() {    
+nextStepButton.addEventListener('click', function () {
 
     numInputBomb = document.getElementById("input-bomb");
     var userNum = parseInt(numInputBomb.value);
 
-    if (userList.length < maxOfNums - bombList.length) {        
-        
-        if (userNum > 0 && userNum <= maxOfNums && !isNaN(userNum)) {
-    
-            if (!userList.includes(userNum)) {
-        
-                if (!bombList.includes(userNum)) {
-                    userList.push(userNum);
-                    userScore = userList.length;                    
-                    outputStillInGame.innerHTML = "Bomba evitata!!!"
+    if (userNum > 0 && userNum <= maxOfNums && !isNaN(userNum)) {
 
-                    if (userList.length === maxOfNums - bombList.length){
-                        //hai vinto, bisogna inserirlo qui xke qui si aggiorna la lunghezza dell'array
-                        gameOver = true;                        
-                    }
-                    
-                } else {
-                    //hai perso
+        if (!userList.includes(userNum)) {
+
+            if (!bombList.includes(userNum)) {
+                userList.push(userNum);
+                userScore = userList.length;
+                outputStillInGame.innerHTML = "Bomba evitata!!!"
+
+                if (userList.length === maxOfNums - bombList.length) {
+                    //hai vinto, bisogna inserirlo qui xke qui si aggiorna la lunghezza dell'array
                     gameOver = true;
-                    nextStep = false;
                 }
-    
+
             } else {
-                alert('Non puoi inserire lo stesso numero')
+                //hai perso
+                gameOver = true;
+                nextStep = false;
             }
-    
+
         } else {
-            alert('Devi inserire un numero fra 1 e ' + maxOfNums);
+            alert('Non puoi inserire lo stesso numero')
         }
+
+    } else {
+        alert('Devi inserire un numero fra 1 e ' + maxOfNums);
     }
 
-    // solo se hai vinto o perso, nascondi quello che non ti serve
-    if (gameOver) { 
 
-        for (var i = 0; i < toHide.length; i++){    
+    // solo se hai vinto o perso, nascondi quello che non ti serve
+    if (gameOver) {
+
+        for (var i = 0; i < toHide.length; i++) {
             toHide[i].style.display = ("none");
         }
-    
-        outputStillInGame.style.padding = ("24px"); 
-    
+
+        outputStillInGame.style.padding = ("24px");
+
         if (nextStep) {
+            //hai vinto
             outputStillInGame.innerHTML = 'Complimenti, Hai vinto!!! <br/> Hai evitato tutte le bombe!!!';
             nextStepButton.disabled = true;
         } else {
+            //hai perso
             outputStillInGame.innerHTML = "Bomba Beccata :( <br/> Hai perso :("
         }
     }
-    
+
     numInputBomb.value = ''; // reset
 
-    outputHtml.innerHTML = userScore;   
+    outputHtml.innerHTML = userScore;
 
 })
